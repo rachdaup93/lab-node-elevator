8f581b2c10b5ab97c452263cadc7150944780e2f
Rachelle Dauphin <rachdaup.tech@gmail.com>
2017-08-29 08:01:47 -0400

    done.

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,103 @@
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
+    // current floor where the elevator is
+    this.floor = 0;
+
+    // top floor of the building (max value of "this.floor")
+    this.MAXFLOOR = 10;
+
+    // current direction that the elevator is moving in
+    this.direction = 'up';
+
+    // an array of numbers of the floors which the elevator must stop
     this.requests   = [];
+
+    // array of person objects that represent people waiting to be picked up
+    this.waitingList = [];
+
+    // array of person objects that represent people on the elevator to be dropped off
+    this.passengers = [];
+
+    // time id for when the elevator is running
+    this.running = 0;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    // begin a setInterval that calls "update()" every second
+    this.running = setInterval(()=>{
+      let nextFloor;
+      if(this.requests.length > 0){
+        nextFloor = this.requests.shift();
+        if(this.floor > nextFloor)
+          this.direction = 'down';
+        else
+          this.direction = 'up';
+        this.floor = nextFloor;
+        console.log(this.requests);
+      }
+      this.update();
+      this._passengersLeave();
+      this._passengersEnter();
+    }, 1000);
+  }
+
+  stop() {
+    // cancel the setInterval started by "start()"
+    clearInterval(this.running);
+  }
+  update(){
+    // (for now) will only call "log()"
+    this.log();
+  }
+  log() {
+    // display the current direction and current floorUp
+    // Examples:
+    // "Direction: down | Floor 3"
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
+  _passengersEnter() {
+    if(this.waitingList.length > 0){
+      this.waitingList.forEach((person)=>{
+        if(person.originFloor === this.floor){
+          console.log(`${person.name} just entered the elevator.`);
+          this.requests.push(person.destinationFloor);
+          this.passengers.push(this.waitingList.shift());
+        }
+      });
+    }
+}
+
+_passengersLeave() {
+  if(this.passengers.length > 0){
+    this.passengers.forEach((person)=>{
+      if(person.destinationFloor === this.floor){
+        console.log(`${person.name} just left the elevator.`);
+        this.requests.push(person.destinationFloor);
+        this.passengers.shift();
+      }
+    });
+  }
+}
+  floorUp() {
+    if(this.floor > 10){
+      console.log("Elevator is at the top floor.");
+      this.floor = 10;
+    }
+    else
+    this.floor++;
+  }
+  floorDown() {
+    if(this.floor < 0){
+      console.log("Elevator is at the ground floor.");
+      this.floor = 0;
+    }
+    else
+    this.floor--;
+  }
+  call(person) {
+    this.waitingList.push(person);
+    this.requests.push(person.originFloor);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,32 @@
+// import the "elevator.js" file into the program
 const Elevator = require('./elevator.js');
+
+// import the "person.js" file into the program
+const Person = require('./person.js');
+
+const myElevator = new Elevator();
+const PersonA = new Person('Ryan', 5, 9);
+const PersonB = new Person('Ernesto', 6, 3);
+const PersonC = new Person('Emma', 1, 4);
+const PersonD = new Person('Allana', 8, 2);
+const PersonE = new Person('Dharma', 2, 10);
+const PersonF = new Person('Danny', 6, 0);
+const PersonG = new Person('Rachelle', 7, 4);
+const PersonH = new Person('Mike', 3, 5);
+
+// start the elevator's automatic movement
+myElevator.start();
+
+// stop the elevator after 20 seconds
+setTimeout(() =>{
+  myElevator.stop();
+}, 27000);
+
+setTimeout(()=> myElevator.call(PersonA), 1000);
+setTimeout(()=> myElevator.call(PersonB), 5000);
+setTimeout(()=> myElevator.call(PersonC), 7000);
+setTimeout(()=> myElevator.call(PersonD), 2000);
+setTimeout(()=> myElevator.call(PersonE), 11000);
+setTimeout(()=> myElevator.call(PersonF), 15000);
+setTimeout(()=> myElevator.call(PersonG), 10000);
+setTimeout(()=> myElevator.call(PersonH), 12000);
